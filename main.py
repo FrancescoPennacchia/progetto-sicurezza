35bab3df502c6b01a483667dc139c6f65342ba71
Francesco Pennacchia <f.pennacchia@studenti.unimol.it>
2023-06-12 22:50:56 +0200

    Forse funziona

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from urllib import request
+import os
 
 
 class MySpider(CrawlSpider):
-    name = 'my_spider'
+    name = 'Spider-sicurezza'
     allowed_domains = ['addons.mozilla.org']
     start_urls = ['https://addons.mozilla.org/it/firefox/extensions/category/search-tools/']
 
@@ -13,8 +15,20 @@ class MySpider(CrawlSpider):
         Rule(LinkExtractor(restrict_css='.Paginate-item--next')),
     )
 
+
     def parse_item(self, response):
+        title = response.css('.AddonTitle::text').get()
         download_link = response.css('.InstallButtonWrapper-download-link::attr(href)').get()
-        # Esegui il download del file utilizzando il link ottenuto
+
+        if title and download_link:
+            file_name = '-'.join(title.split()) + '.xpi'  # Aggiungi trattini tra i diversi spazi
+            folder_path = 'test'  # Percorso della cartella di destinazione
+            file_path = os.path.join(folder_path, file_name)  # Percorso completo del file di destinazione
+
+            os.makedirs(folder_path, exist_ok=True)  # Crea la cartella se non esiste
+
+            request.urlretrieve(download_link, file_path)
+            self.log(f"File {file_name} scaricato correttamente nella cartella {folder_path}.")
 
         yield None
+
