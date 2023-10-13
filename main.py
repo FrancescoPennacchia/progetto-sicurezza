ffce2c48b4e1de3900a6b9f3358414682689598c
Francesco Pennacchia <francescopennacchia@libero.it>
2023-10-13 07:01:17 +0200

    Rimozione normalize

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import os
 class MySpider(CrawlSpider):
     name = 'Spider-sicurezza'
     allowed_domains = ['addons.mozilla.org']
-    start_urls = ['https://addons.mozilla.org/it/firefox/extensions/category/appearance/']
+    start_urls = ['https://addons.mozilla.org/it/firefox/extensions/category/language-support/']
 
     rules = (
         Rule(LinkExtractor(restrict_css='.SearchResult-link'), callback='parse_item'),
@@ -29,7 +29,6 @@ class MySpider(CrawlSpider):
         # Rimuovi i caratteri non ASCII dal nome del file
         normalized_name = ''.join(c for c in name if unicodedata.category(c) != 'Mn')
         # Sostituisci i caratteri non consentiti con _
-        normalized_name = normalized_name.replace("'", "_")
         return normalized_name
 
     def parse_item(self, response):
@@ -37,14 +36,14 @@ class MySpider(CrawlSpider):
         download_link = response.css('.InstallButtonWrapper-download-link::attr(href)').get()
 
         if title and download_link:
-            file_name = '_'.join(title.split()) + '.xpi'  # Aggiungi trattini tra i diversi spazi
-            file_name = self.normalize_filename(file_name)
-            folder_path = 'aspetto'  # Percorso della cartella di destinazione
+            file_name = ' '.join(title.split()) + '.xpi'  # Aggiungi trattini tra i diversi spazi
+            # file_name = self.normalize_filename(file_name)
+            folder_path = 'language-support_2'  # Percorso della cartella di destinazione
             file_path = os.path.join(folder_path, file_name)  # Percorso completo del file di destinazione
 
             os.makedirs(folder_path, exist_ok=True)  # Crea la cartella se non esiste
 
             request.urlretrieve(download_link, file_path)
-            self.log(f"File {file_name} scaricato correttamente nella cartella {folder_path}.")
+            self.log("File {file_name} scaricato correttamente nella cartella {folder_path}.")
 
         yield None
